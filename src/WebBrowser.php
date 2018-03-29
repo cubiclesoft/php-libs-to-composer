8414c7d4dc5cd084822f465e5967bbc0a77618b5
cubiclesoft <webmaster@cubiclesoft.com>
2018-03-29 01:30:39 -0700

    Updated.

--- src/WebBrowser.php
+++ src/WebBrowser.php
@@ -184,6 +184,7 @@
 						$result["url"] = $state["url"];
 						unset($state["options"]["files"]);
 						unset($state["options"]["body"]);
+						unset($state["tempoptions"]["headers"]["Content-Type"]);
 						$result["options"] = $state["options"];
 						$result["firstreqts"] = $state["startts"];
 						$result["numredirects"] = $state["numredirects"];
