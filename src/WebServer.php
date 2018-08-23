2e0595f3e376ecf1982495d96f07c90acbd28972
cubiclesoft <webmaster@cubiclesoft.com>
2018-08-23 01:30:38 -0700

    Updated.

--- src/WebServer.php
+++ src/WebServer.php
@@ -655,7 +655,7 @@
 						$client->mode = "init_response";
 						$client->responseheaders = array();
 						$client->responsefinalized = false;
-						$client->responsebodysize = false;
+						$client->responsebodysize = true;
 
 						$client->httpstate["type"] = "request";
 						$client->httpstate["startts"] = microtime(true);
@@ -737,10 +737,10 @@
 					{
 						if ($client->responsefinalized)
 						{
-							$client->AddResponseHeader("Content-Length", (string)strlen($client->writedata), true);
+							if ($client->responsebodysize !== false)  $client->AddResponseHeader("Content-Length", (string)strlen($client->writedata), true);
 							$client->httpstate["bodysize"] = strlen($client->writedata);
 						}
-						else if ($client->responsebodysize !== false)
+						else if ($client->responsebodysize !== true)
 						{
 							$client->AddResponseHeader("Content-Length", (string)$client->responsebodysize, true);
 							$client->httpstate["bodysize"] = $client->responsebodysize;
@@ -1047,6 +1047,8 @@
 					);
 
 					if (!isset($codemap[$code]))  $code = 500;
+
+					if ($this->responsebodysize === true && (($code >= 100 && $code < 200) || $code === 204 || $code === 304))  $this->responsebodysize = false;
 
 					$code = $code . " " . $codemap[$code];
 				}
