3c125e40b4454acba5cea658f15db13de1b15870
cubiclesoft <webmaster@cubiclesoft.com>
2021-05-23 01:30:33 -0700

    Updated.

--- src/HTTP.php
+++ src/HTTP.php
@@ -6,8 +6,8 @@
 
 	class HTTP
 	{
-		// RFC 3986 delimeter splitting implementation.
-		public static function ExtractURL($url)
+		// RFC 3986 delimeter splitting implementation with IDNA/Punycode support.
+		public static function ExtractURL($url, $idnahost = true)
 		{
 			$result = array(
 				"scheme" => "",
@@ -118,6 +118,25 @@
 					}
 
 					$result["host"] = $url;
+
+					// Handle conversion to Punycode for IDNs.
+					if ($idnahost)
+					{
+						$y = strlen($result["host"]);
+						for ($x = 0; $x < $y && ord($result["host"][$x]) <= 0x7F; $x++);
+
+						if ($x < $y)
+						{
+
+
+							$host = \CubicleSoft\UTFUtils::ConvertToPunycode($result["host"]);
+							if ($host !== false)
+							{
+								$result["orighost"] = $result["host"];
+								$result["host"] = $host;
+							}
+						}
+					}
 				}
 			}
 
@@ -1625,6 +1644,11 @@
 			if ($useproxy && !$proxyconnect && $proxyusername != "")  $data .= "Proxy-Authorization: BASIC " . base64_encode($proxyusername . ":" . $proxypassword) . "\r\n";
 			if ($username != "")  $data .= "Authorization: BASIC " . base64_encode($username . ":" . $password) . "\r\n";
 			$ver = explode(".", $options["httpver"]);
+			if (isset($options["headers"]["Host"]))
+			{
+				$url2 = self::ExtractURL("http://" . $options["headers"]["Host"]));
+				$options["headers"]["Host"] = $url2["host"] . (isset($url2["port"]) && $url2["port"] != "" ? ":" . $url2["port"] : "");
+			}
 			if ((int)$ver[0] > 1 || ((int)$ver[0] == 1 && (int)$ver[1] >= 1))
 			{
 				if (!isset($options["headers"]["Host"]))  $options["headers"]["Host"] = $host . ($defaultport ? "" : ":" . $port);
