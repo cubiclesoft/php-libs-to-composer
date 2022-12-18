ec8bc10d2b773a63e28280a479cf8359ca2e71bb
cubiclesoft <webmaster@cubiclesoft.com>
2022-12-18 01:30:18 -0700

    Updated.

--- src/DiscordSDK.php
+++ src/DiscordSDK.php
@@ -66,7 +66,7 @@
 				)
 			);
 
-			if ($method === "POST" || $method === "PUT")
+			if ($method === "POST" || $method === "PUT" || $method === "PATCH")
 			{
 				$options2["headers"]["Content-Type"] = "application/json";
 				$options2["body"] = json_encode($postvars, JSON_UNESCAPED_SLASHES);
